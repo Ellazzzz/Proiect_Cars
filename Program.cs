56f90fed2c3b277d224f25eb4b7339987534dd94
Ellazzzz <prusumar@gmail.com>
2023-11-19 00:12:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_Cars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("mustang", "blue", 2010, 220);
+            Car Opel = new Car("astra", "red", 2020, 180);
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Opel.year);
+            Console.WriteLine(Ford.color);
+            Ford.fullThrottle();
+
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.year);
+            Console.WriteLine(Ford.color);
+            Opel.fullThrottle();
+            Console.ReadKey();
+        }
+    }
+}
